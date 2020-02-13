51f5d6b635f47fb20a60f2c8d05a1547745722d3
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-02-13 16:16:49 +0100

    Add some dependencies to avoid racing conditions
    [3.1.x]
    
    ERM 18233
    
    Change-Id: I8f7f719aaeccdb0474a5ec92cc9f6bb16cffc72d

--- resources/bs.nsfrc/ui/plugin/MWMediaDialog.js
+++ resources/bs.nsfrc/ui/plugin/MWMediaDialog.js
@@ -1,56 +1,58 @@
-bs.util.registerNamespace( 'bs.nsfrc.ui.plugin' );
+( function( mw, $, bs ) {
+	bs.util.registerNamespace( 'bs.nsfrc.ui.plugin' );
 
-bs.nsfrc.ui.plugin.MWMediaDialog = function BsNsfrcUiPluginMWMediaDialog( component, options ) {
-	bs.nsfrc.ui.plugin.MWMediaDialog.super.apply( this, [ component ] );
+	bs.nsfrc.ui.plugin.MWMediaDialog = function BsNsfrcUiPluginMWMediaDialog( component, options ) {
+		bs.nsfrc.ui.plugin.MWMediaDialog.super.apply( this, [ component ] );
 
-	options = options || {};
-	options = $.extend( {
-			currentNamespace: 0,
-			invalidNamespaces: [],
-			formattedNamespaces: []
-		},
-		options
-	);
+		options = options || {};
+		options = $.extend( {
+				currentNamespace: 0,
+				invalidNamespaces: [],
+				formattedNamespaces: []
+			},
+			options
+		);
 
-	this.currentNamespace = options.currentNamespace;
-	this.invalidNamespaces = options.invalidNamespaces;
-	this.formattedNamespaces = options.formattedNamespaces;
-};
+		this.currentNamespace = options.currentNamespace;
+		this.invalidNamespaces = options.invalidNamespaces;
+		this.formattedNamespaces = options.formattedNamespaces;
+	};
 
-OO.inheritClass( bs.nsfrc.ui.plugin.MWMediaDialog, bs.vec.ui.plugin.MWMediaDialog );
+	OO.inheritClass( bs.nsfrc.ui.plugin.MWMediaDialog, bs.vec.ui.plugin.MWMediaDialog );
 
-bs.nsfrc.ui.plugin.MWMediaDialog.prototype.setNewUploadBooklet = function() {
+	bs.nsfrc.ui.plugin.MWMediaDialog.prototype.setNewUploadBooklet = function() {
 
-	this.namespaceSelector = new mw.widgets.NamespaceInputWidget( {
-		value: this.currentNamespace,
-		exclude: this.invalidNamespaces
-	} );
-	this.namespaceSelectorLayout = new OO.ui.FieldLayout( this.namespaceSelector, {
-		label: mw.msg( "bs-nsfrc-uploaddialog-selector-label" ),
-		align: 'top'
-	} );
+		this.namespaceSelector = new mw.widgets.NamespaceInputWidget( {
+			value: this.currentNamespace,
+			exclude: this.invalidNamespaces
+		} );
+		this.namespaceSelectorLayout = new OO.ui.FieldLayout( this.namespaceSelector, {
+			label: mw.msg( "bs-nsfrc-uploaddialog-selector-label" ).text(),
+			align: 'top'
+		} );
 
 
-	if ( this.component.mediaUploadBooklet instanceof bs.vec.ui.ForeignStructuredUpload.BookletLayoutOneClick ) {
-		this.component.mediaUploadBooklet.uploadForm.items[0].addItems( [ this.namespaceSelectorLayout ], 0 );
-	}
+		if ( this.component.mediaUploadBooklet instanceof bs.vec.ui.ForeignStructuredUpload.BookletLayoutOneClick ) {
+			this.component.mediaUploadBooklet.uploadForm.items[0].addItems( [ this.namespaceSelectorLayout ], 0 );
+		}
 
-	else {
-		//instanceof bs.vec.ui.ForeignStructuredUpload.BookletLayout or bs.vec.ui.ForeignStructuredUpload.BookletLayoutSimple
-		this.component.mediaUploadBooklet.infoForm.items[1].addItems( [ this.namespaceSelectorLayout ], 0 );
-	}
+		else {
+			//instanceof bs.vec.ui.ForeignStructuredUpload.BookletLayout or bs.vec.ui.ForeignStructuredUpload.BookletLayoutSimple
+			this.component.mediaUploadBooklet.infoForm.items[1].addItems( [ this.namespaceSelectorLayout ], 0 );
+		}
 
-	this.component.mediaUploadBooklet.on( 'getfilename', this.onMediaUploadBookletGetFilename, [], this );
-};
+		this.component.mediaUploadBooklet.on( 'getfilename', this.onMediaUploadBookletGetFilename, [], this );
+	};
 
-bs.nsfrc.ui.plugin.MWMediaDialog.prototype.onMediaUploadBookletGetFilename = function( booklet, data ) {
-	var selectedNamespace = +this.namespaceSelector.getValue();
-	var prefix = '';
-	if ( this.formattedNamespaces[selectedNamespace] ) {
-		prefix = this.formattedNamespaces[selectedNamespace];
-	}
-	if ( prefix !== '' ) {
-		prefix = prefix + ':';
-	}
-	data.filename = prefix + data.filename;
-};
+	bs.nsfrc.ui.plugin.MWMediaDialog.prototype.onMediaUploadBookletGetFilename = function( booklet, data ) {
+		var selectedNamespace = +this.namespaceSelector.getValue();
+		var prefix = '';
+		if ( this.formattedNamespaces[selectedNamespace] ) {
+			prefix = this.formattedNamespaces[selectedNamespace];
+		}
+		if ( prefix !== '' ) {
+			prefix = prefix + ':';
+		}
+		data.filename = prefix + data.filename;
+	};
+} )( mediaWiki, jQuery, blueSpice );
