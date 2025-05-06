69d6529a5cabe688e9a9b310abe4e139580ab98a
hslater <hamish.slater@hallowelt.com>
2025-05-06 19:30:12 +1200

    eslint
    ERM20742
    
    [REL1_43, REL1_43-5.0.x, master]
    
    Change-Id: Ie98c956781a962f88f7e3b602469972bc9ba1cb4

--- resources/mediawiki.Title.newFromImg.js
+++ resources/mediawiki.Title.newFromImg.js
@@ -1,32 +1,32 @@
-(function( mw, $ ) {
-	var oldTitleNewFromImg = mw.Title.newFromImg;
+( function ( mw ) {
+	const oldTitleNewFromImg = mw.Title.newFromImg;
 
 	mw.Title.newFromImg = function ( img ) {
-		var title = oldTitleNewFromImg( img );
-		var newTitle = null;
-		var dummyNSFRTitle = null;
-		var src = img.jquery ? img[0].src : img.src;
+		const title = oldTitleNewFromImg( img );
+		let newTitle = null;
+		let dummyNSFRTitle = null;
+		let src = img.jquery ? img[ 0 ].src : img.src;
 		src = decodeURI( src );
 
-		matches = src.match( /\/([^\s]*\/{1})([^\s]*\/{1})([0-9]+?)\/[a-f0-9]\/[a-f0-9]{2}\/([^\s]+)$/ );
+		let matches = src.match( /\/([^\s]*\/{1})([^\s]*\/{1})([0-9]+?)\/[a-f0-9]\/[a-f0-9]{2}\/([^\s]+)$/ );
 		// If file is e.g. "20210121121500!Some_file.png/100px-Some_file.png", we need to remove the timestamp
-		if ( matches !== null && matches[2] === 'archive/' ) {
+		if ( matches !== null && matches[ 2 ] === 'archive/' ) {
 			// Is it an archived thumbnail?
-			var fileNameParts = matches[4].match( /([0-9]{14})!([^\s]+)\/([^\s]+)$/ );
+			let fileNameParts = matches[ 4 ].match( /([0-9]{14})!([^\s]+)\/([^\s]+)$/ );
 
 			if ( fileNameParts === null ) {
 				// Is it an archived image?
-				fileNameParts = matches[4].match( /([0-9]{14})!([^\s]+)$/ );
+				fileNameParts = matches[ 4 ].match( /([0-9]{14})!([^\s]+)$/ );
 			}
 
 			if ( fileNameParts !== null ) {
-				dummyNSFRTitle = mw.Title.newFromText( 'dummy', matches[3] );
+				dummyNSFRTitle = mw.Title.newFromText( 'dummy', matches[ 3 ] );
 				newTitle = mw.Title.newFromText(
-					'File:' + dummyNSFRTitle.getNamespacePrefix() + fileNameParts[2]
+					'File:' + dummyNSFRTitle.getNamespacePrefix() + fileNameParts[ 2 ]
 				);
 
 				// Hint: The timestamp prefix of the filename is the upload date of the newer file.
-				newTitle.timestamp = fileNameParts[1];
+				newTitle.timestamp = fileNameParts[ 1 ];
 
 				return newTitle;
 			}
@@ -34,15 +34,15 @@
 			// Is it an archived thumb?
 			matches = src.match( /\/([^\s]*\/{1})([0-9]+?)\/[a-f0-9]\/[a-f0-9]{2}\/([^\s]+)\/([^\s]+)/ );
 
-			if ( matches === null) {
+			if ( matches === null ) {
 				// Is it an archived image?
 				matches = src.match( /\/([^\s]*\/{1})([0-9]+?)\/[a-f0-9]\/[a-f0-9]{2}\/([^\s]+)/ );
 			}
 
-			if ( matches !== null) {
-				dummyNSFRTitle = mw.Title.newFromText( 'dummy', matches[2] );
+			if ( matches !== null ) {
+				dummyNSFRTitle = mw.Title.newFromText( 'dummy', matches[ 2 ] );
 				newTitle = mw.Title.newFromText(
-					'File:' + dummyNSFRTitle.getNamespacePrefix() + matches[3]
+					'File:' + dummyNSFRTitle.getNamespacePrefix() + matches[ 3 ]
 				);
 
 				return newTitle;
@@ -50,5 +50,5 @@
 		}
 
 		return title;
-	}
-})( mediaWiki, jQuery );
+	};
+}( mediaWiki ) );
